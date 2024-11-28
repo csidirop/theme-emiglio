bf2c69ab87e5632ffa08d0c35c0f397fa695c4d6
Christos Sidiropoulos <dev@csidirop.de>
2024-11-28 16:19:54 +0000

    Use `ConnectedCarousel` to show the images
    Signed-off-by: Christos Sidiropoulos <dev@csidirop.de>

--- items/show.php
+++ items/show.php
@@ -1,45 +1,54 @@
-<?php echo head(array('title' => metadata($item, array('Dublin Core', 'Title')), 'bodyclass' => 'items show')); ?>
+<?php
+    echo head(array('title' => metadata($item, array('Dublin Core', 'Title')), 'bodyclass' => 'items show'));
+?>
 
 <h1><?php echo metadata($item, array('Dublin Core', 'Title')); ?></h1>
 
 <div id="primary">
-    <?php if ((get_theme_option('Item FileGallery') == 0) && metadata('item', 'has files')): ?>
-    <div id="itemfiles" class="element">
-        <?php echo files_for_item(array('imageSize' => 'fullsize')); ?>
-    </div>
+<?php if ((get_theme_option('Item FileGallery') == 0) && metadata('item', 'has files')): ?>
+        <div id="itemfiles" class="element">
+            <?php echo $this->shortcodes('[concarousel ids='.metadata('item','id').' 
+                                        center=false 
+                                        slides=5
+                                        showdescription=false 
+                                        captionposition=right 
+                                        width=100% 
+                                        float=right 
+                                        slideshow=false 
+                                        speed=2000 
+                                        focus=true 
+                                        navigation=true 
+                                        navbar=false]');
+            ?>
+        </div>
     <?php endif; ?>
-
     <?php echo all_element_texts($item); ?>
-
     <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
-
 </div><!-- end primary -->
 
 <div id="secondary">
-
     <!-- The following returns all of the files associated with an item. -->
     <?php if ((get_theme_option('Item FileGallery') == 1) && metadata($item, 'has files')): ?>
-    <div id="itemfiles" class="element">
-        <h2><?php echo __('Files'); ?></h2>
-        <div class="element-text"><?php echo item_image_gallery(); ?></div>
-    </div>
+        <div id="itemfiles" class="element">
+            <h2><?php echo __('Files'); ?></h2>
+            <div class="element-text"><?php echo item_image_gallery(); ?></div>
+        </div>
     <?php endif; ?>
-    
-
-
     <!-- The following prints a list of all tags associated with the item -->
     <?php if (metadata($item, 'has tags')): ?>
-    <div id="item-tags" class="element">
-        <h2><?php echo __('Tags'); ?></h2>
-        <div class="element-text tags"><?php echo tag_string('item'); ?></div>
-    </div>
+        <div id="item-tags" class="element">
+            <h2><?php echo __('Tags'); ?></h2>
+            <div class="element-text tags"><?php echo tag_string('item'); ?></div>
+        </div>
     <?php endif; ?>
 
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->
     <?php if (get_collection_for_item()): ?>
         <div id="collection" class="element">
             <h2><?php echo __('Collection'); ?></h2>
-            <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+            <div class="element-text">
+                <p><?php echo link_to_collection_for_item(); ?></p>
+            </div>
         </div>
     <?php endif; ?>
 
@@ -48,7 +57,6 @@
         <h2><?php echo __('Citation'); ?></h2>
         <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
     </div>
-
 </div><!-- end secondary -->
 
 <ul class="item-pagination navigation">
